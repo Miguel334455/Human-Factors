85c2488cf3b98f4415ac2bfd0fb6ff5065e5f7da
Miguel334455 <mangelramirez.rp@gmail.com>
2025-03-30 18:56:15 -0500

    base

---
+++ music-catalogue/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { InicioComponent } from '../inicio/inicio.component';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [InicioComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
